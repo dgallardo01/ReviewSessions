ecb021e9d5b2d1a1ae2fbdf3ee88a2f7ef459ac5
Daniel Gallardo <daniel.gallardo@flatironschool.com>
2014-02-10 17:43:07 -0500

    Added 2-10 lecture notes

---
+++ 2-10/FISAddressBook/FISAddressBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FISAddressBook
+//
+//  Created by Danny on 2/10/14.
+//  Copyright (c) 2014 Danny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
